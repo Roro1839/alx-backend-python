b04a49727391b738eec9bd37ad6aefa1a6c2bcd0
Roro1839 <ronaldokipkirui90@gmail.com>
2023-10-09 00:45:20 +0300

    variable annotation

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env pyhton3
+from typing import List
+"""a function to add floats"""
+
+
+def sum_list(input_list: List) -> float:
+    """a function to add floats"""
+    return float(sum(input_list))

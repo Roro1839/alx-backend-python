1f802f428f254f2906bb267c89993635ab397c9a
Roro1839 <ronaldokipkirui90@gmail.com>
2023-10-12 04:01:26 +0300

    python

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Generators using yield"""
+import asyncio
+import random
+from typing import List
+
+
+async def async_generator() -> List[float]:
+    """Coroutine will loop 10 times, each time asynchronously wait 1 second,"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Couroutine that will exercute async_comprehension asychronously"""
+
+
+from time import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measuring time for for runs of async_comprehension"""
+    start = time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    return time() - start
